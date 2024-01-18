2afe98c74e9745ccd1f32d2da29361d1cd9fcd3c
pdemirov <krustachka@gmail.com>
2024-01-18 17:35:58 +0200

    update simple.py

--- simple.py
+++ simple.py
@@ -1,8 +1,15 @@
 # simple python script to play around with git commands
-
+import random
 #sort array
 arr = [5, 1, 3, 2, 4]
+
 print(arr)
 
+random.shuffle(arr)
+
+print(arr)
 for x in arr:
     print(x)
+    if x == 5:
+        print('yes, it have 5 ')
+        break
